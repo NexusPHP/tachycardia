00e451186dfe7f4927787331e51cc72dd9d6d830
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-08-24 04:14:51 +0800

    Fix multiline promoted properties

--- src/Subscriber/TestRunner/ExecutionFinishedSubscriber.php
+++ src/Subscriber/TestRunner/ExecutionFinishedSubscriber.php
@@ -22,7 +22,10 @@ use PHPUnit\Event;
  */
 final class ExecutionFinishedSubscriber implements Event\TestRunner\ExecutionFinishedSubscriber
 {
-    public function __construct(private readonly SlowTestCollection $collection, private readonly Renderer $renderer) {}
+    public function __construct(
+        private readonly SlowTestCollection $collection,
+        private readonly Renderer $renderer,
+    ) {}
 
     public function notify(Event\TestRunner\ExecutionFinished $event): void
     {
